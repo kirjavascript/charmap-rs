879a8bd805d704cb0238ed2f0923567faee429fa
kirjavascript <snkenjoi@gmail.com>
2022-11-09 20:51:19 +0000

    hover UI and stuff

--- src/clipboard.rs
+++ src/clipboard.rs
@@ -1,6 +1,7 @@
 use copypasta_ext::prelude::*;
 use copypasta_ext::x11_fork::ClipboardContext;
 
+// ui.output().copied_text doesnt persist after close
 pub fn set(input: String) {
     let mut ctx = ClipboardContext::new().unwrap();
     ctx.set_contents(input).unwrap();
--- src/main.rs
+++ src/main.rs
@@ -28,24 +28,45 @@ impl Default for CharMap {
     }
 }
 
-// left clie to add, right click to copy and close
+// left clie to add, dblclick to copy and close
 // custom charcode
+// emoji
+// kaomoji
 // hover charcode info
 
 impl eframe::App for CharMap {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
 
-                ui.add(egui::TextEdit::singleline(&mut self.input).desired_width(999.0));
-
             match self.mode {
                 Mode::Emoji => {
 
-                    egui::ScrollArea::vertical()
-                        .show_viewport(ui, |ui, viewport| {
-                            ui.label((9786..20000).map(|c| char::from_u32(c).unwrap()).collect::<String>());
-                        });
+                    //         ui.label((9786..20000).map(|c| char::from_u32(c).unwrap()).collect::<String>());
+                    let font_id = egui::FontId::proportional(45.0);
+                    egui::ScrollArea::vertical().show(ui, |ui| {
+                        ui.horizontal_wrapped(|ui| {
+                            ui.spacing_mut().item_spacing = egui::Vec2::splat(2.0);
 
+                            for chr in (0x1f600..0x1f620).map(|c| char::from_u32(c).unwrap()) {
+
+                                let button = egui::Button::new(
+                                    egui::RichText::new(chr.to_string()).font(font_id.clone()),
+                                )
+                                    .frame(false);
+
+                                let tooltip_ui = |ui: &mut egui::Ui| {
+                                    ui.label(
+                                        egui::RichText::new(chr.to_string()).font(font_id.clone()),
+                                    );
+                                    ui.label(format!("{}\nU+{:X}\n\nClick to copy", "name", chr as u32));
+                                };
+
+                                if ui.add(button).on_hover_ui(tooltip_ui).clicked() {
+                                    clipboard::set(self.input.to_owned());
+                                }
+                            }
+                        });
+                    });
 
                 },
             }
